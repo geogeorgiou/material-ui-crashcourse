f7940ee701b2a02553225938b00396e6887f8b19
geogeorgiou <georgiogeo27@gmail.com>
2021-03-17 15:33:53 +0200

    Fixing the package.json, creating a MuiTheme

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import Header from "./ui/Header";
+import {ThemeProvider} from "@material-ui/styles";
+
+import theme from './ui/Theme';
 
 function App() {
 
     return (
         <div className="App">
 
-            <Header/>
-            {[...new Array(120)]
-                .map(
-                    () => `Cras mattis consectetur purus sit amet fermentum.
+            <ThemeProvider theme={theme}>
+                <Header/>
+                {[...new Array(120)]
+                    .map(
+                        () => `Cras mattis consectetur purus sit amet fermentum.
                         Cras justo odio, dapibus ac facilisis in, egestas eget quam.
                         Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
                         Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-                )
-                .join('\n')}
+                    )
+                    .join('\n')}
+            </ThemeProvider>
+
 
         </div>
     );
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,11 +2,10 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 function ElevationScroll(props) {
-    const { children } = props;
+    const {children} = props;
     // Note that you normally won't need to set the window ref as useScrollTrigger
     // will default to window.
     // This is only being set here because the demo is in an iframe.
@@ -22,16 +21,12 @@ function ElevationScroll(props) {
 
 export default function Header(props) {
     return (
-        <React.Fragment>
-            <CssBaseline />
-            <ElevationScroll {...props}>
-                <AppBar>
-                    <Toolbar>
-                        <Typography variant="h6">Scroll to Elevate App Bar</Typography>
-                    </Toolbar>
-                </AppBar>
-            </ElevationScroll>
-            <Toolbar />
-        </React.Fragment>
+        <ElevationScroll {...props}>
+            <AppBar position="fixed" >
+                <Toolbar>
+                    <Typography variant="h6">Netu Development</Typography>
+                </Toolbar>
+            </AppBar>
+        </ElevationScroll>
     );
 }
