d36ffdcb75972eeb5a28d2fe49f3ac4d843be3a2
geogeorgiou <georgiogeo27@gmail.com>
2021-03-19 12:55:27 +0200

    Responsive design for Buttons and Animation at LandingPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Lottie from 'react-lottie';
-import {makeStyles} from "@material-ui/styles";
+import {makeStyles, useTheme} from "@material-ui/styles";
 import animationData from '../animations/landinganimation/data';
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
@@ -14,7 +14,58 @@ const useStyles = makeStyles(theme => ({
         maxWidth: "50em",
         minWidth: "21em",
         marginTop: "2em",
-        marginLeft: "10%"
+        marginLeft: "10%",
+        [theme.breakpoints.down('sm')]: {
+            maxWidth: '30em'
+        },
+    },
+
+    estimateButton: {
+        ...theme.typography.estimate,
+        backgroundColor: theme.palette.common.orange,
+        borderRadius: 50,
+        height: 45,
+        width: 145,
+        marginRight: 40,
+        "&:hover": {
+            backgroundColor: theme.palette.secondary.light
+        }
+
+    },
+
+    buttonContainer: {
+        marginTop: '1em'
+    },
+
+    learnButtonHero: {
+        borderColor: theme.palette.common.blue,
+        color: theme.palette.common.blue,
+        borderWidth: 2,
+        textTransform: "none",
+        borderRadius: 50,
+        fontFamily: "Roboto",
+        fontWeight: "bold",
+        fontSize: "0.9rem",
+        height: 45,
+        width: 145
+    },
+
+    mainContainer: {
+        marginTop: '5em',
+        [theme.breakpoints.down('md')]: {
+            marginTop: '3em'
+        },
+        [theme.breakpoints.down('xs')]: {
+            marginTop: '2em'
+        }
+    },
+
+    heroTextContainer: {
+        minWidth: "21.5em",
+        marginLeft: "1em",
+        [theme.breakpoints.down('xs')]: {
+            marginLeft: 0
+        }
     }
 
 }));
@@ -31,26 +82,35 @@ const defaultOptions = {
 export default function LandingPage() {
 
     const classes = useStyles();
+    const theme = useTheme();
 
     return (
-        <Grid container direction="column">
+        <Grid container direction="column" className={classes.mainContainer}>
             <Grid item>
                 <Grid container justify="flex-end" alignItems="center" direction="row">
 
                     {/*IMPORTANT use sm to share the same space until the given breakpoint (sm)*/}
-                    <Grid sm item>
+                    <Grid sm item className={classes.heroTextContainer}>
                         <Typography variant="h2" align="center">
                             Bringing West Coast Technology
                             <br/> to the Midwest
                         </Typography>
-                        <Grid container>
+                        <Grid container justify="center" className={classes.buttonContainer}>
                             <Grid item>
-                                <Button variant="contained">Free Estimate</Button>
+                                <Button
+                                    className={classes.estimateButton}
+                                    variant="contained"
+                                >
+                                    Free Estimate
+                                </Button>
                             </Grid>
                             <Grid item>
-                                <Button variant="outlined">
-                                    Learn More
-                                    <ButtonArrow width={15} height={15} fill="red"/>
+                                <Button
+                                    className={classes.learnButtonHero}
+                                    variant="outlined"
+                                >
+                                    <span style={{marginRight: 10}}>Learn More</span>
+                                    <ButtonArrow width={15} height={15} fill={theme.palette.common.blue}/>
                                 </Button>
                             </Grid>
                         </Grid>
